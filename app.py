60aa15244f7fdfc6e824f498aa8ce3a934d737d6
Almerei <130736883+Almerei@users.noreply.github.com>
2023-12-12 14:04:41 +0000

    full_website

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_bakers_from_db
+from database import load_bakers_from_db,load_job_from_db
 
 app = Flask(__name__)
 
@@ -14,7 +14,20 @@ def  hello_world():
 @app.route("/api/jobs")
 def list_jobs():
   bakers = load_bakers_from_db()
-  return jsonify(JOBS)
+  return jsonify(bakers)
+
+# Ваш код Flask-приложения
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    return render_template ('menu.html',
+                           job=job)
+
+@app.route("/cart")
+def show_cart():
+    # Add logic to render the cart page
+    return render_template('cart.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -1,15 +1,16 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = os.environ['DB_CONECTTION_STRING']
 
-engine = create_engine(
-    db_connection_string,
-    connect_args={
-        "ssl": {
-            "ssl_ca": "/etc/ssl/cert.pem"
-        }
-    })
+db_connection_string = os.environ['DB_CONECTTION_STRING']
+print(f"DB Connection String: {db_connection_string}")
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+# ... rest of your code ...
 
 
 def load_bakers_from_db():
@@ -19,3 +20,18 @@ def load_bakers_from_db():
     for row in result.all():
       bakers.append(row)
     return bakers
+
+
+# ... (остальной код)
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+        text("SELECT * FROM bakery WHERE id = :val").params(val=id))
+    row = result.fetchone()
+
+  if row is None:
+    return None
+  else:
+    return row
